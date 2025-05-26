6972c3b41b064d53d873f7d26da27c7730ae99de
kraksyx <blahblahusdme@gmail.com>
2025-05-27 00:51:15 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+const questions = [
+  {
+    "question": "An operator shall ensure, that regarding a special VFR flight:",
+    "options": [
+      "the vertical distance from clouds is more than 250 m.",
+      "the flight visibility is not less than 3 km.",
+      "the flight visibility below 10 000 ft (3 050 m) is not less than 5 km.",
+      "the horizontal distance from clouds is more than 1 000 m."
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Flight crew members on the flight deck shall keep their safety belt fastened:",
+    "options": [
+      "only during take-off and landing and whenever necessary by the commander in the interest of safety.",
+      "only during take-off and landing.",
+      "from take-off to landing.",
+      "while at their station."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "One shall not initiate any flight made in accordance with instrument flight rules unless the available information indicates that the conditions at the aerodrome of predicted destination or, at an aerodrome of alternative destination, are: (EASA AIR OPS)",
+    "options": [
+      "at the predicted time of take-off equal to or better than the minimum conditions required for aerodrome use.",
+      "at the predicted time of arrival, and for a reasonable period before and after such a predicted time, equal to or better than the minimum conditions required for aerodrome use.",
+      "at the predicted time of arrival better than the minimum conditions required for aerodrome use.",
+      "at the predicted time of arrival equal to or better than the minimum conditions required for aerodrome use."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "The period of validity of an operator proficiency check is:",
+    "options": [
+      "2 years",
+      "6 months",
+      "12 months",
+      "18 months"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Which statement is true regarding emergency medical kits?",
+    "options": [
+      "They must be located on the flight deck.",
+      "They must be readily accessible for use.",
+      "They must be available in the rear galley, away from the passengers.",
+      "They must be accessible to the cabin crew."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Special categories of passengers (SCPs) or persons with reduced mobility (PRMs) should be allocated to specific seats. Which statement is correct?",
+    "options": [
+      "PRMs should not be placed in a seat which would restrict any exit path in the case of an emergency unless accompanied by an able bodied adult.",
+      "PRMs should be placed in a seat which would be most comfortable for themselves because they have specific requirements.",
+      "PRMs should always be placed in the middle of the aircraft.",
+      "PRMs should not be placed in a seat which would restrict any exit path in the case of an emergency."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "What manuals are to be carried on board the aircraft?",
+    "options": [
+      "Operations Manual in total.",
+      "Company instructions for all flight crew.",
+      "All those specified in the Certificate of Airworthiness.",
+      "Relevant parts of the ops manual and AFM."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Above what flight level must one pilot wear an oxygen mask at all times during commercial flight?",
+    "options": [
+      "250",
+      "490",
+      "410",
+      "300"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "The planning minima for a destination alternate aerodrome with a circling approach available is:",
+    "options": [
+      "circling visibility, MDH and ceiling for the period ETA +/- 1 hour.",
+      "circling visibility plus 1 000 m, MDH and ceiling plus 200 ft for the period ETA +/- 1 hour.",
+      "circling visibility, MDH and ceiling at ETA.",
+      "NPA minima at ETA +/- 1 hour."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "According to EASA AIR OPS, what is the planning minima for a destination aerodrome, if it is equipped with a non-precision approach only?",
+    "options": [
+      "Non-precision minima (RVR and the ceiling at or above MDH) plus 200 ft / 1 000 m.",
+      "RVR / visibility specified in accordance with CAT.OP.MPA.110.",
+      "The ceiling at or above MDH.",
+      "Circling minima."
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Who is a key post holder within a company?",
+    "options": [
+      "Safety manager & safety review board",
+      "Safety pilot & safety manager",
+      "Safety standard manager & safety manager",
+      "Safety manager"
+    ],
+    "correct": 3
+  }
+];
