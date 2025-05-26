4c2c980db3a8bb2704f0c70c77c6d49bea34ef19
kraksyx <blahblahusdme@gmail.com>
2025-05-27 00:59:40 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,282 @@
+const questions = [
+  {
+    "question": "An operator shall ensure, that regarding a special VFR flight:",
+    "options": [
+      "the vertical distance from clouds is more than 250 m.",
+      "the flight visibility is not less than 3 km.",
+      "the flight visibility below 10 000 ft (3 050 m) is not less than 5 km.",
+      "the horizontal distance from clouds is more than 1 000 m."
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Flight crew members on the flight deck shall keep their safety belt fastened:",
+    "options": [
+      "only during take-off and landing and whenever necessary by the commander in the interest of safety.",
+      "only during take-off and landing.",
+      "from take-off to landing.",
+      "while at their station."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "One shall not initiate any flight made in accordance with instrument flight rules unless the available information indicates that the conditions at the aerodrome of predicted destination or, at an aerodrome of alternative destination, are: (EASA AIR OPS)",
+    "options": [
+      "at the predicted time of take-off equal to or better than the minimum conditions required for aerodrome use.",
+      "at the predicted time of arrival, and for a reasonable period before and after such a predicted time, equal to or better than the minimum conditions required for aerodrome use.",
+      "at the predicted time of arrival better than the minimum conditions required for aerodrome use.",
+      "at the predicted time of arrival equal to or better than the minimum conditions required for aerodrome use."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "The period of validity of an operator proficiency check is:",
+    "options": [
+      "2 years",
+      "6 months",
+      "12 months",
+      "18 months"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Which statement is true regarding emergency medical kits?",
+    "options": [
+      "They must be located on the flight deck.",
+      "They must be readily accessible for use.",
+      "They must be available in the rear galley, away from the passengers.",
+      "They must be accessible to the cabin crew."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "Special categories of passengers (SCPs) or persons with reduced mobility (PRMs) should be allocated to specific seats. Which statement is correct?",
+    "options": [
+      "PRMs should not be placed in a seat which would restrict any exit path in the case of an emergency unless accompanied by an able bodied adult.",
+      "PRMs should be placed in a seat which would be most comfortable for themselves because they have specific requirements.",
+      "PRMs should always be placed in the middle of the aircraft.",
+      "PRMs should not be placed in a seat which would restrict any exit path in the case of an emergency."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "What manuals are to be carried on board the aircraft?",
+    "options": [
+      "Operations Manual in total.",
+      "Company instructions for all flight crew.",
+      "All those specified in the Certificate of Airworthiness.",
+      "Relevant parts of the ops manual and AFM."
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Above what flight level must one pilot wear an oxygen mask at all times during commercial flight?",
+    "options": [
+      "250",
+      "490",
+      "410",
+      "300"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "The planning minima for a destination alternate aerodrome with a circling approach available is:",
+    "options": [
+      "circling visibility, MDH and ceiling for the period ETA +/- 1 hour.",
+      "circling visibility plus 1 000 m, MDH and ceiling plus 200 ft for the period ETA +/- 1 hour.",
+      "circling visibility, MDH and ceiling at ETA.",
+      "NPA minima at ETA +/- 1 hour."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "According to EASA AIR OPS, what is the planning minima for a destination aerodrome, if it is equipped with a non-precision approach only?",
+    "options": [
+      "Non-precision minima (RVR and the ceiling at or above MDH) plus 200 ft / 1 000 m.",
+      "RVR / visibility specified in accordance with CAT.OP.MPA.110.",
+      "The ceiling at or above MDH.",
+      "Circling minima."
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Who is a key post holder within a company?",
+    "options": [
+      "Safety manager & safety review board",
+      "Safety pilot & safety manager",
+      "Safety standard manager & safety manager",
+      "Safety manager"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "4.complexity of the aircraft used and associated risks.",
+    "options": [
+      "1, 3, 4",
+      "1, 4",
+      "1, 3",
+      "2, 3"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "What minimum rest period shall be provided before a flight duty which starts away from the crewmember\u2019s home base?",
+    "options": [
+      "8 hours",
+      "12 hours",
+      "10 hours",
+      "14 hours"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "What skills constitute pilot proficiency checks?",
+    "options": [
+      "Simulator flying skills.",
+      "The ability to land safely.",
+      "Flying technique, emergency procedures and IFR.",
+      "The ability to conform with set procedures."
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Which of the following needs approval?",
+    "options": [
+      "Anything on the flight deck.",
+      "Child harness used in cabin.",
+      "Nothing on the flight deck.",
+      "A time piece."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "What does the definition of a hostile environment refer to?",
+    "options": [
+      "A built environment.",
+      "A safe forced landing cannot be accomplished because the surface is inadequate.",
+      "Overwater operations, overhead the open sea areas north of 45S and south of 45N.",
+      "Any operation overhead the open sea."
+    ],
+    "correct": 1
+  },
+  {
+    "question": "According to AIR OPS on an IFR flight a commander shall not commence take-off unless the expected weather conditions at the destination and/or required alternate aerodrome(s) are at or above:",
+    "options": [
+      "the planning minima, at the estimated time of arrival.",
+      "the aerodrome operating minima, at the estimated time of arrival, and for a reasonable period before and after such a predicted time.",
+      "the aerodrome operating minima, during a period from 1 hour before to 1 hour after the estimated time of arrival at the aerodrome.",
+      "the planning minima, during a period from 1 hour before to 1 hour after the estimated time of arrival at the aerodrome."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "According to EASA AIR OPS, what is the planning minima for a destination alternate with a CAT II approach available?",
+    "options": [
+      "Non-precision minima plus 200 ft / 1 000 m.",
+      "Circling minima.",
+      "RVR according to CAT I.",
+      "rect: C"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "According to EASA AIR OPS, what is the planning minima for a destination alternate with a non-precision approach available?",
+    "options": [
+      "Non-precision minima (RVR and the ceiling at or above MDH) plus 1 000 m / 200 ft.",
+      "Non-precision minima (RVR and the ceiling at or above MDH).",
+      "RVR according to CAT I.",
+      "Circling minima."
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The commander of a public transport aircraft must ensure that passenger seat belts are secure for:",
+    "options": [
+      "take-off, landing, taxiing and turbulence.",
+      "take-off, landing, taxiing, turbulence and during emergencies.",
+      "take-off and landing.",
+      "take-off, landing and turbulence."
+    ],
+    "correct": 1
+  },
+  {
+    "question": "What does abbreviation MASPS mean?",
+    "options": [
+      "Maximum Approved Seating Propagation Standards",
+      "Minimum Aviation System Performance Standards",
+      "Minimum Airport Specifications and Personnel Standards",
+      "Maximum Approved System Power and Specifications"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "In a ditching situation, the passenger life jackets will be inflated:",
+    "options": [
+      "once the passengers are in the water",
+      "immediately after the opening of the exits",
+      "immediately on ditching",
+      "when leaving the aircraft"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "According to EASA AIR OPS, what is the planning minima for a destination aerodrome, if it is equipped with a non-precision approach only?",
+    "options": [
+      "Non-precision minima (RVR and the ceiling at or above MDH) plus 200 ft / 1 000 m",
+      "The ceiling at or above MDH",
+      "RVR / visibility specified in accordance with CAT.OP.MPA.110 and the ceiling at or above MDH",
+      "Circling minima"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "ICAO Annex 6:",
+    "options": [
+      "the flight preparation stage for a turbo-propeller aircraft, the landing distance at an alternate aerodrome shall be less than the available landing distance multiplied by a factor of:",
+      "0.6",
+      "0.5",
+      "0.8"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Above what altitude is it mandatory for one of the flight deck crew members to wear an oxygen mask?",
+    "options": [
+      "37 000 ft",
+      "32 000 ft",
+      "41 000 ft",
+      "25 000 ft"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "According to EASA AIR OPS, what is the planning minima for a destination aerodrome, if it is equipped with a non-precision approach only?",
+    "options": [
+      "Non-precision minima (RVR and the ceiling at or above MDH) plus 200 ft / 1 000 m",
+      "The ceiling at or above MDH",
+      "RVR / visibility specified in accordance with CAT.OP.MPA.110",
+      "Circling minima"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "For turbo-jet aircraft, in the flight preparation stage, the landing distance at the scheduled destination aerodrome shall be less than the available landing distance multiplied by a factor of:",
+    "options": [
+      "0.6",
+      "0.7",
+      "0.5",
+      "0.8"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "With regard to the management system an operator is required to establish, implement and maintain, such system shall correspond to the:",
+    "options": [
+      "complexity of operation and size of the company.",
+      "annual income level of the company.",
+      "annual profit/loss and the number of passengers carried.",
+      "complexity of the aircraft used and associated risks."
+    ],
+    "correct": 1
+  }
+];
